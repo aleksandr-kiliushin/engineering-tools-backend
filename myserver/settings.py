8045f40614debeddaaece61f8fcc06dc82047aa1
oaiyui <alexklshn@gmail.com>
2021-02-10 23:50:35 +0300

    push to heroku

--- myserver/settings.py
+++ myserver/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bl7wvwq&rw!j^8w4e4@a*!trgd_*q&jprao#lt538w4hzhmq&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
