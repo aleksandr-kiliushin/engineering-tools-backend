84129dc60c8fd72e34f3a61a65515f6f170519c2
oaiyui <alexklshn@gmail.com>
2021-02-13 16:05:04 +0300

    deleted 'discount_group' from api response

--- valves/serializers.py
+++ valves/serializers.py
@@ -9,7 +9,7 @@ class EquipmentSerializer(serializers.ModelSerializer):
         model = Equipment
         # fields = ('code', 'dn', 'equip_type', 'full_title', 'kvs', 'price', 'type_title', 'z', 'discount_group')
         # fields = '__all__'
-        exclude = ('id',)
+        exclude = ('id', 'discount_group')
 
     def to_representation(self, instance):
         result = super(EquipmentSerializer, self).to_representation(instance)
